333dfd6856654bec693299e3a18ad0f5be41b3c0
Ghivalza Harviansyah <hrvnsyh.ghivalza@gmail.com>
2022-11-26 13:47:55 +0700

    comment route

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// User
 Route::get('/user', [UserController::class,'index'])->name('user');
 Route::get('userRegistration',[UserController::class,'create'])->name('registrasi');
 Route::get('/userView/{user}', [UserController::class,'show'])->name('userView');
@@ -31,6 +32,7 @@ Route::get('/userEdit/{user}', [UserController::class,'edit'])->name('userEdit')
 Route::post('/userUpdate/{user}', [UserController::class,'update'])->name('userUpdate');
 Route::get('/userDelete/{user}', [UserController::class,'destroy'])->name('userDelete');
 
+//Collections
 Route::get('/koleksi', [CollectionsController::class,'index'])->middleware(['auth', 'verified'])->name('koleksi');
 Route::get('koleksiTambah',[CollectionsController::class,'create'])->middleware(['auth', 'verified'])->name('koleksiTambah');
 Route::get('/koleksiView/{collection}', [CollectionsController::class,'show'])->middleware(['auth', 'verified'])->name('koleksiView');
